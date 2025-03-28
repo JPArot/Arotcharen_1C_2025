4a8c8c7fcc4c21a9edf4f93b23b1444d8918a7da
Jean Pierre Arotcharen <jeanpierrearotcharen@gmail.com>
2025-03-28 16:21:13 -0300

    Actualizado

--- firmware/projects/guia1_ej456/main/guia1_456.c
+++ firmware/projects/guia1_ej456/main/guia1_456.c
@@ -19,14 +19,33 @@
  * |:----------:|:-----------------------------------------------|
  * | 12/09/2023 | Document creation		                         |
  *
- * @author Albano Peñalva (albano.penalva@uner.edu.ar)
+ * @author Jean Pierre Arotcharen (jean.arotcharen@ingenieria.uner.edu.ar)
  *
  */
 
 /*==================[inclusions]=============================================*/
 #include <stdio.h>
 #include <stdint.h>
+ #include <led.h>
+ #include "freertos/FreeRTOS.h"
+ #include "freertos/task.h"
 /*==================[macros and definitions]=================================*/
+
+int8_t  convertToBcdArray (uint32_t data, uint8_t digits, uint8_t * bcd_number)
+{
+
+    // Con este for separo en centena decena unidad
+	for(uint_t i=0; i<digits; i++){
+		bcd_number[digits-1-i]= data%10; 
+		data=data/10;
+	}
+        /*bcd_number[0]= data%10;
+		 bcd_number[1]=(data/10)%10;
+		 bcd_number[2]=(data/100)%10;
+*/ 
+   return 0;
+}
+
 
 /*==================[internal data definition]===============================*/
 
@@ -34,6 +53,14 @@
 
 /*==================[external functions definition]==========================*/
 void app_main(void){
-	printf("Hello world!\n");
+	uint32_t number= 123;
+	uint8_t digitos= 3;
+	uint8_t arreglo [3];
+	convertToBcdArray(number,digitos,&arreglo);
+
+	printf("El primer digito vale %d \n" , arreglo[0]);
+	printf("El segundo digito vale %d \n" , arreglo[1]);
+	printf("El tercer digito vale %d \n" , arreglo[2]);
+	
 }
 /*==================[end of file]============================================*/
